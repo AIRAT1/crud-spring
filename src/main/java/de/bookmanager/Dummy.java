e64f73587290bf2c3e523087936ad775cb77cbb4
Ayrat Hairullin <ayrat1@mail.ru>
2020-04-19 22:48:43 +0300

    q

--- src/main/java/de/bookmanager/Dummy.java
+++ src/main/java/de/bookmanager/Dummy.java
@@ -1,4 +1,15 @@
 package de.bookmanager;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class Dummy {
+    private int dummyInt;
+    private String dummyString;
 }
