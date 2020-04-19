3a80e48abd22fe9bede57d97162a730581511897
Ayrat Hairullin <ayrat1@mail.ru>
2020-04-19 22:25:00 +0300

    project complete

---
+++ src/main/java/de/bookmanager/model/Book.java
@@ -0,0 +1,24 @@
+package de.bookmanager.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "BOOKS")
+@Data
+public class Book {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "BOOK_TITLE")
+    private String bookTitle;
+
+    @Column(name = "BOOK_AUTHOR")
+    private String bookAuthor;
+
+    @Column(name = "BOOK_PRICE")
+    private int price;
+}
